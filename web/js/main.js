970e0691e8a4bdb9c5c15cc58beab632a96b831e
SparrowSSSS <vlad22832006@gmail.com>
2022-12-31 11:27:38 +0300

    panel v.1.0

--- web/js/main.js
+++ web/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $('.block3').click(function () {
-        if ($('.block3').html() == '<i class="fa-solid fa-eye"></i>') {
+        if ($(this).html() == '<i class="fa-solid fa-eye"></i>') {
             $(this).html('<i class="fa-sharp fa-solid fa-eye-slash"></i>');
             $('.pass').attr('type', 'text');
         } else {
@@ -16,5 +16,35 @@ $(document).ready(function () {
     });
     $('.h3').click(function () {
         $('.i3').focus();
+    });
+    $("*").focus(function () {
+        if ($('#r').attr('class') == 'block') {
+            $('#r').addClass('none');
+            $('#r').removeClass('block');
+        };
+    });
+    $(".pass").focus(function () {
+        $('#r').addClass('block');
+        $('#r').removeClass('none');
+    });
+    $('.sign_up').click(function () {
+        if ($('.sign_up').attr('class') == 'sign_up') {
+            $('.sign_in').removeClass('active');
+            $('.sign_up').addClass('active');
+            $('.s_in').addClass('none');
+            $('.s_in').removeClass('block');
+            $('.s_up').addClass('block');
+            $('.s_up').removeClass('none');
+        };
+    });
+    $('.sign_in').click(function () {
+        if ($('.sign_in').attr('class') == 'sign_in') {
+            $('.sign_up').removeClass('active');
+            $('.sign_in').addClass('active');
+            $('.s_up').addClass('none');
+            $('.s_up').removeClass('block');
+            $('.s_in').addClass('block');
+            $('.s_in').removeClass('none');
+        };
     });
 });
